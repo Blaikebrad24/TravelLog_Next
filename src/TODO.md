ac2c8bbd43e4c4c8873fafd1c2e32eaa92a1e628
Blaike Bradford <bb494v@att.com>
2023-02-24 12:59:35 -0600

    Added tailwindcss and daisyui

--- src/TODO.md
+++ src/TODO.md
@@ -2,16 +2,16 @@
     *[x]Install Dependencies
     *[x]Install/SetupLinter
     *[x]Setup Express App
-    *[x]Setup Not Found and Error Middleware
+    *[]Setup Not Found and Error Middleware
 *[x]Model Mongo DB -> start MongoDB(brew services start mongodb-community@6.0) end MongoDB (brew services stop mongodb-community@6.0), connect(mongosh)
     *What will we store?
-*[x]Setup Postgres model(s)
+*[x]Setup MongoDB model(s)
 *[x]Post/logs
     *Ceate a new log entry
 *[x]GET/logs
     *List all log entries
-*[x] Setup Client
-*[x] Create Form to add new entry
-*[x] Setup Map SDK on client
-*[x] List all log entries on map
-*[x] DEPLOY!
+*[] Setup Client
+*[] Create Form to add new entry
+*[] Setup Map SDK on client
+*[] List all log entries on map
+*[] DEPLOY!
--- src/components/TravelLogForm.tsx
+++ src/components/TravelLogForm.tsx
@@ -8,15 +8,22 @@ export default function TravelLogForm() {
   const [title, setTitle] = useState('');
   return (
     <form>
-      <label>Title</label>
-      <input
-        value={title}
-        onChange={(e) => {
-          setTitle(e.currentTarget.value);
-          // console.log(title);
-        }}
-      />
-      <button>Create</button>
+      <div className="form-control w-full max-w-xs">
+        <label className="label">
+          <span className="label-text">Title</span>
+        </label>
+        <input
+          type="text"
+          className="input input-bordered input-primary w-full max-w-xs"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.currentTarget.value);
+            // console.log(title);
+          }}
+        />
+      </div>
+
+      <button className="btn btn-success">Create</button>
     </form>
   );
 }
