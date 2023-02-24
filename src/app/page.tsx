c09d545530790bc96a77629d5a991e8a8d3e2b2c
Blaike Bradford <bb494v@att.com>
2023-02-23 21:11:20 -0600

    The first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default async function Home() {
   const logs = await TravelLogs.find().toArray(); // querying database here
   return (
     <main>
-      <h1>Hello World, this is the travel log app | FullStack | Nextjs | MongoDB</h1>
+      <h1>
+        Hello World, this is the travel log app | FullStack | Nextjs | MongoDB
+      </h1>
       <h2>There are {logs.length} in the database</h2>
       {logs.map((log) => (
         <div key={log._id.toString()}>{log.title}</div>
